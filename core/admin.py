8fa3f646beef30fc08cd7ea20041f0c27bd00ddf
MARCIO SILVINO SOUZA DA MOTA <MSSM@wilson.sons>
2021-10-08 13:52:17 -0300

    Meu segundo commit

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from core.models import Evento #importar a nova classe eventos criada no models.py
 
 # Register your models here.
+
+#exibir mais
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo','data_evento')
+    list_filter = ('usuario', 'titulo',) #virgula tem q estar no final
+
+#registrar a nova classe eventos criada no models.py
+admin.site.register(Evento , EventoAdmin) #associar também a nova EventoAdmin
--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User #importar para usar a User que já existe no Django
 
 # Create your models here.
+
+#nome da minha classe (que irá virar uma tabela no banco de dados)
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)  #tipo char com no máximo 100 caracteres
+    descricao = models.TextField(blank=True,null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True) #insere automaticamente a data/hora da criação
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) #utilizar aqui a User
+
+    # para criar a tabela com o nome abaixo e não com o default que o Python cria que será core_evento
+    class Meta:
+        db_table = 'evento'
+
+    #para exibir o nome do titulo no evento e não evento_object1
+    def __str__(self):
+        return self.titulo
